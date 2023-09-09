2514a594dd1667fd1ebf3dc7e93b28028e12460e
svtter <svtter@qq.com>
2023-09-09 22:13:25 +0800

    feat: add command but not work, permission denied

--- HelloWorld2/ViewController.swift
+++ HelloWorld2/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var nameField: NSTextField!
     @IBOutlet weak var helloLabel: NSTextField!
+    
+    @IBOutlet weak var postNameField: NSTextField!
+    
+    let blogDir: String = "/opt/homebrew/bin/hugo"
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,6 +35,59 @@ class ViewController: NSViewController {
         let greeting = "Hello \(name)!"
         print(greeting)
         helloLabel.stringValue = greeting
+    }
+    
+    // create new post
+    @IBAction func newPostClicked(_ sender: Any) {
+        let postName = postNameField.stringValue
+        if postName.isEmpty {
+            helloLabel.stringValue = "No Post Name"
+        }
+        
+        let task = Process()
+        let fileManager = FileManager.default
+        let hugoPath = "hugo"
+        
+        // 检查是否存在 hugo
+        if !fileManager.fileExists(atPath: hugoPath) {
+            print("hugo file not exist.")
+        }
+        
+        task.executableURL = URL(fileURLWithPath: hugoPath)
+        task.arguments = ["new", postName]
+        task.currentDirectoryURL = URL(fileURLWithPath: blogDir)
+        
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        task.standardOutput = outputPipe
+        task.standardError = errorPipe
+        
+        do {
+            try task.run()
+            task.waitUntilExit()
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func listPostClicked(_ sender: Any) {
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: "/usr/local/bin/code")
+        task.arguments = [blogDir,]
+        
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        task.standardOutput = outputPipe
+        task.standardError = errorPipe
+        
+        do {
+            try task.run()
+            task.waitUntilExit()
+        } catch {
+            print(error)
+            print("task run error.")
+        }
+        
     }
 }
 
